7e0ba7e3a78ff00c6a7cc1c6faa48e7eb78ced6c
Steve Loveless <steve.loveless@gmail.com>
2011-07-05 17:48:51 -0700

    Added first test for #project_by_name

--- spec/test_linker/wrapper_spec.rb
+++ spec/test_linker/wrapper_spec.rb
@@ -37,6 +37,69 @@ XML
     end
   end
   
+  describe "#project_by_name" do
+    context "TestLink API 1.0 Beta 5" do
+      it "should raise an error that it's not supported'" do
+        pending
+      end
+    end
+    
+    context "TestLink API 1.0" do
+      it "gets a project by its name" do
+        body = <<-XML
+<?xml version=\"1.0\"?>
+<methodResponse>
+  <params>
+    <param>
+      <value>
+        <array><data>
+  <value><struct>
+  <member><name>id</name><value><string>123</string></value></member>
+  <member><name>notes</name><value><string></string></value></member>
+  <member><name>color</name><value><string></string></value></member>
+  <member><name>active</name><value><string>1</string></value></member>
+  <member><name>prefix</name><value><string>meow</string></value></member>
+  <member><name>tc_counter</name><value><string>1899</string></value></member>
+  <member><name>is_public</name><value><string>1</string></value></member>
+  <member><name>options</name><value><string>O:8:&quot;stdClass&quot;:4:{s:19:&quot;requirementsEnabled&quot;;s:1:&quot;1&quot;;s:19:&quot;testPriorityEnabled&quot;;s:1:&quot;1&quot;;s:17:&quot;automationEnabled&quot;;s:1:&quot;1&quot;;s:16:&quot;inventoryEnabled&quot;;b:0;}</string></value></member>
+  <member><name>name</name><value><string>Meow</string></value></member>
+  <member><name>opt</name><value><struct>
+  <member><name>requirementsEnabled</name><value><string>1</string></value></member>
+  <member><name>testPriorityEnabled</name><value><string>1</string></value></member>
+  <member><name>automationEnabled</name><value><string>1</string></value></member>
+  <member><name>inventoryEnabled</name><value><boolean>0</boolean></value></member>
+</struct></value></member>
+</struct></value>
+</data></array>
+      </value>
+    </param>
+  </params>
+</methodResponse>
+
+        XML
+
+        register_body(body)
+        @tl.project_by_name("Meow").should == [
+            { :id=>123,
+              :notes=>"",
+              :color=>"",
+              :active=>1,
+              :prefix=>"meow",
+              :tc_counter=>1899,
+              :is_public=>1, 
+              :options=>"O:8:\"stdClass\":4:{s:19:\"requirementsEnabled\";s:1:\"1\";s:19:\"testPriorityEnabled\";s:1:\"1\";s:17:\"automationEnabled\";s:1:\"1\";s:16:\"inventoryEnabled\";b:0;}",
+              :name=>"Meow", 
+              :opt=> { :requirementsEnabled=>1,
+                  :testPriorityEnabled=>1, 
+                  :automationEnabled=>1, 
+                  :inventoryEnabled=>false
+              }
+        }
+        ]
+      end
+    end
+  end
+
   describe "#projects" do
     it "gets a list of projects on the server" do
       body = <<-XML
